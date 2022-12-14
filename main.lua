ec8eb5b8004ab0ab8b0dc52ed5e93ccae4b5bc71
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-14 12:03:51 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,19 @@
+local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
+local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 
+if game.PlaceId == 4924922222 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Brookhaven/main.lua", true))()
+
+elseif game.PlaceId == 155615604 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Prison-Life/main.lua", true))()
+
+else
+
+Notification:Notify(
+    {Title = "Error", Description = "Game Not Supported!"},
+    {OutlineColor = Color3.fromRGB(255, 0, 0),Time = 5.5, Type = "default"}
+)
+
+end
