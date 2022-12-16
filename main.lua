1225f1de50753c4761e285b5768cad1a954aae6d
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-17 00:41:16 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -13,6 +13,10 @@ elseif game.PlaceId == 10679961467 then
 
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/50-50-Pick-A-Door-Obby/main.lua", true))()
 
+elseif game.PlaceId == 10875701453 then
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Edward-the-Man-Eating-Train/main.lua"))()
+
 else
 
 Notification:Notify(
