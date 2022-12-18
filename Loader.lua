25cd0f50586faa092c433db4f135012a89e58f22
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2022-12-18 13:48:54 +0800

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,34 +1,12 @@
-local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
-local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
+local list = {
+    ["4924922222"] = "https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Brookhaven/main.lua",
+    ["155615604"] = "https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Prison-Life/main.lua",
+    ["10679961467"] = "https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/50-50-Pick-A-Door-Obby/main.lua",
+    ["10875701453"] = "https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Edward-the-Man-Eating-Train/main.lua",
+}
 
-if game.PlaceId == 4924922222 then
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Brookhaven/main.lua", true))()
-
-elseif game.PlaceId == 155615604 then
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Prison-Life/main.lua", true))()
-
-elseif game.PlaceId == 10679961467 then
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/50-50-Pick-A-Door-Obby/main.lua", true))()
-
-elseif game.PlaceId == 10875701453 then
-
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Games/Edward-the-Man-Eating-Train/main.lua"))()
-
-else
-
-Notification:Notify(
-    {Title = "Error", Description = "Game Not Supported!"},
-    {OutlineColor = Color3.fromRGB(128, 187, 219),Time = 5.5, Type = "default"}
-)
-wait(.5)
-Notification:Notify(
-    {Title = "Astral Hub | Universal", Description = "Loaded!"},
-    {OutlineColor = Color3.fromRGB(128, 187, 219),Time = 5.5, Type = "default"}
-)
-wait(.1)
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Vcsk/AstralHub/main/Universal.lua", true))()
-
+for id, url in next, list do
+    if string.find(id, game.PlaceId) then
+        loadstring(game:HttpGet(url))(); break
+    end
 end
