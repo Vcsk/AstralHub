e711c3773f621273eb43ee93d227e726312a1d6e
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-01-05 16:27:53 +0800

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -2,8 +2,4 @@ return {
     ["vcsk0"] = true,
     ["TOXIC_SOLOZ"] = true,
     ["AstralX0"] = true,
-    ["XXMALAYSIA_GGG"] = true,
-    ["Iman00916"] = true,
-    ["troll23408"] = true,
-    ["aperopoh"] = true,
 }
