abf4b36799295a3c85bf88be9a83c58ad31bd84b
vcsk0 <111982858+Vcsk@users.noreply.github.com>
2023-01-14 11:19:14 +0800

    Update GodBullet.lua

--- Games/Da-Hood/GodBullet.lua
+++ Games/Da-Hood/GodBullet.lua
@@ -1,3 +1,6 @@
-if game.Players.LocalPlayer.Character.BodyEffects:FindFirstChild("Armor") then
-	game.Players.LocalPlayer.Character.Armor:Destroy()
-end
+game.Players.LocalPlayer.Character.BodyEffects.Armor:Destroy()
+Armor = Instance.new("IntValue", player.Character.BodyEffects)
+Armor.Name = 'Armor'
+Armor.Value = 100
+GodLabel = Instance.new('IntValue', Armor)
+GodLabel.Name = 'God'
